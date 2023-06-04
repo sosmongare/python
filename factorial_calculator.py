4d2351cc23d190be81d6a0783794583c6bf9771f
Sospeter Mongare <sosmongare@gmail.com>
2023-06-04 20:29:37 +0300

    Added factorial calculator script

---
+++ factorial_calculator.py
@@ -0,0 +1,14 @@
+"""
+Level 1
+
+Question: Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+x=int(input("Enter a number to compute the factorial of:"))
+print("The factorial of", x, "is", fact(x))
