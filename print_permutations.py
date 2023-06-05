d4ce672bb173e41c70db9f8121bccee10667f7bd
Sospeter Mongare <sosmongare@gmail.com>
2023-06-05 22:58:50 +0300

    Modified code to allow user input

--- print_permutations.py
+++ print_permutations.py
@@ -3,5 +3,24 @@ Please write a program which prints all permutations of [1,2,3]
 
 Hints: Use itertools.permutations() to get permutations of list.
 """
+# import itertools
+# print(list(itertools.permutations([1,2,3])))
+
+"""
+Allw user to input the numbers to generate permutations.
+"""
+
 import itertools
-print(list(itertools.permutations([1,2,3])))
+
+# Prompt the user to enter numbers separated by commas
+print ("---Permutation Calculator!---")
+user_input = input("Enter numbers separated by commas: ")
+
+# Split the input string into individual numbers and convert them to integers
+numbers = [int(num) for num in user_input.split(",")]
+
+# Generate permutations using itertools.permutations()
+permutations = list(itertools.permutations(numbers))
+
+# Print the permutations
+print("Permutations:", permutations)
