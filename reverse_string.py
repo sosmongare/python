e0dda5a04397ef9fc4b22ebeb6f011a8d1e1583f
Sospeter Mongare <sosmongare@gmail.com>
2023-06-05 23:04:16 +0300

    Add script to reverse string

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+print("--- Reverse String Script ---")
+# Accept a string input from the user
+user_input = input("Enter a string to reverse: ")
+
+# Reverse the string using slicing
+reversed_string = user_input[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
