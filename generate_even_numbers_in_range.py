3f00f0e76760dcded2474337fcf3029d8e4fe0de
Sospeter Mongare <sosmongare@gmail.com>
2023-06-19 22:08:16 +0300

    script to generate even numbers between a range

---
+++ generate_even_numbers_in_range.py
@@ -0,0 +1,8 @@
+"""
+Generate a Python list of all the even numbers between 4 to 30
+Hint:
+Use the built-in function range() to generate the sequence of numbers between the given start number to the stop number with a step = 2 to get even numbers.
+pass range() function to a list constructor to create a list
+"""
+
+print(list(range(4, 30, 2)))
