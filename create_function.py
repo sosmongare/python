ed9772700659a4b8213cb583be01f3bead79cbc8
Sospeter Mongare <sosmongare@gmail.com>
2023-06-20 23:40:46 +0300

    script tp create a function that takes two arguments

---
+++ create_function.py
@@ -0,0 +1,16 @@
+"""
+Write a program to create a function that takes two arguments, name and age, and print their value.
+Show Hint
+
+    Use the def keyword with the function name to define a function.
+    Next, take two parameters
+    Print them using the print() function
+    Call function by passing name and age
+"""
+# demo is the function name
+def demo(name, age):
+    # print value
+    print(name, age)
+
+# call function
+demo("Sos", 25)
