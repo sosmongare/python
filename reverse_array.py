10cfd4c3ca40c290736ee88de1ff8643cd8d202e
Sospeter Mongare <sosmongare@gmail.com>
2023-06-22 00:06:29 +0300

    Script to reverse an array

--- reverse_array.py
+++ reverse_array.py
@@ -1,3 +1,6 @@
+"""
+In this example, we use the [::-1] slicing technique on the numbers array to create a reversed version of the array called reversed_numbers. When the [::-1] slice is applied, it returns a new list with elements in reverse order. Finally, we print the reversed_numbers list, which represents the original array in reverse order.
+"""
 numbers = [5, 2, 9, 1, 7]
 
 # Print the array in reverse order
